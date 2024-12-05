10e77b2495d1cc107599864fe310135c2ef93054
evansnicholas <evansnicholas@hotmail.com>
2024-12-05 12:47:34 +0000

    Cleaning up debug code.

--- hilomods/software/gen_app/sd_card.ino
+++ hilomods/software/gen_app/sd_card.ino
@@ -30,9 +30,6 @@ const uint8_t SD_CS_PIN = SDCARD_SS_PIN;
 #define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
 #endif  // HAS_SDIO_CLASS
 
-
-bool DEBUG_SD_CARD = true;
-
 SdFs sd;
 FsFile settingsFile;
 
@@ -47,9 +44,6 @@ void setupSDCard() {
     Serial.println("Could not connect to SD card");
     sd.initErrorHalt(&Serial);
     return;
-  }
-  if (DEBUG_SD_CARD) {
-    printSDCardInfo();
   }
 }
 
@@ -141,54 +135,4 @@ bool parseLine(char* str) {
   }
   Serial.println("Setting not recognized");
   return false;
-}
-
-void printSDCardInfo() {
-//   Serial.println();
-//   int sdcardinit = card.init(SPI_HALF_SPEED, SDSS);
-//   Serial.print("Card type:         ");
-//   switch (card.type()) {
-//     case SD_CARD_TYPE_SD1:
-//      Serial.println("SD1");
-//      break;
-//     case SD_CARD_TYPE_SD2:
-//      Serial.println("SD2");
-//      break;
-//     case SD_CARD_TYPE_SDHC:
-//      Serial.println("SDHC");
-//      break;
-//     default:
-//      Serial.println("Unknown");
-//   }
-//   // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-//   if (!volume.init(card)) {
-//    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-//    return;
-//   }
-//   Serial.print("Clusters:          ");
-//   Serial.println(volume.clusterCount());
-//   Serial.print("Blocks x Cluster:  ");
-//   Serial.println(volume.blocksPerCluster());
-//   Serial.print("Total Blocks:      ");
-//   Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-//   Serial.println();
-//   // print the type and size of the first FAT-type volume
-//   uint32_t volumesize;
-//   Serial.print("Volume type is:    FAT");
-//   Serial.println(volume.fatType(), DEC);
-//   volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-//   volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-//   volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-//   Serial.print("Volume size (Kb):  ");
-//   Serial.println(volumesize);
-//   Serial.print("Volume size (Mb):  ");
-//   volumesize /= 1024;
-//   Serial.println(volumesize);
-//   Serial.print("Volume size (Gb):  ");
-//   Serial.println((float)volumesize / 1024.0);
-//   Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-//   root.openRoot(volume);
-//   // list all files in the card with date and size
-//   root.ls(LS_R | LS_DATE | LS_SIZE);
-//   root.close();
 }
